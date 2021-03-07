90aeb451fe4df358e984574b27f0433ceafe6be5
SharkZ <z592029820@gmail.com>
2021-03-07 17:25:03 +0800

    Update <Buffer>

--- MyHTTP/http/Buffer.cpp
+++ MyHTTP/http/Buffer.cpp
@@ -2,16 +2,29 @@
 #include <unistd.h>
 #include <malloc.h>
 #include <memory.h>
+#include <sys/uio.h>
 
-Buffer::Buffer(int fd) :m_fd(fd),m_buffPos(0), m_buffSize(2048),m_nextbuffPos(0){
+/*
+		//n
+		//returnType function(...){};
+
+	n=0		->		funtion was uncomplete;
+	n=1		->		funtion was complete;
+	n=1-0	->		function was complete,but isn't prefect;
+*/
+
+//0
+Buffer::Buffer(int fd) :m_fd(fd), m_buffRWPos(0),m_buffPos(0), m_buffSize(2048),m_nextbuffPos(0){
 	nextbuff_ = nullptr;
 };
 
+//0
 Buffer::~Buffer() {
 	if (nextbuff_ != nullptr) delete[]nextbuff_;
 };
 
-int Buffer::read2buff() {
+//0
+int Buffer::read2fd() {
 	int ntoread=0;
 	if ((ntoread = read(m_fd, &buff_[m_buffPos], m_buffSize-m_buffPos)) < 0) {
 		//error
@@ -21,7 +34,53 @@ int Buffer::read2buff() {
 	return ntoread;//true
 };
 
-int Buffer::write2buff(void* writebuff,int writeSize) {
+//10
+int Buffer::write2fd() {
+	int ntoWrite=0;
+	int buffAbsWrite = m_buffPos - m_buffRWPos;
+	if (nextbuff_ != nullptr) {		//存在备用buff_,使用集中写
+		int nextbuffAbsWrite = m_nextbuffPos - m_nextbuffRWPos;
+		struct iovec temp_iovec[2];
+		temp_iovec[0].iov_base = &buff_[m_buffRWPos];
+		temp_iovec[0].iov_len = buffAbsWrite;
+		temp_iovec[1].iov_base = &nextbuff_[m_buffRWPos];
+		temp_iovec[1].iov_len = nextbuffAbsWrite;
+		if (ntoWrite=writev(m_fd, temp_iovec, 2) < 0) return -1;		//error
+		if (ntoWrite!=buffAbsWrite+nextbuffAbsWrite) {	//没全部写完,更新buff指针
+			if (ntoWrite >= buffAbsWrite) {	//buff_缓冲区写完，nextbuff_没有完全写完
+				m_buffRWPos = m_buffPos = 0;
+				ntoWrite -= buffAbsWrite;	//nextbuff_已写入内核的数据大小
+				m_nextbuffRWPos += ntoWrite;
+				write2fd();
+				return 0;
+			}
+			else {
+				m_buffRWPos += ntoWrite;
+				write2fd();
+				return 0;
+			}
+		}//全部写完
+		return 0;
+	}
+	//只存在默认buff_
+	else if (ntoWrite = write(m_fd, &buff_[m_buffRWPos], buffAbsWrite) < 0) {	
+		return -1;		//write error
+	}
+	else if (ntoWrite != buffAbsWrite) {
+		m_buffRWPos += ntoWrite;
+		write2fd();
+		return 0;
+	}
+	return 0;
+};
+
+//0
+int Buffer::readBuff(void* readbuff, int readSize) {
+
+};
+
+//0
+int Buffer::writeBuff(void* writebuff,int writeSize) {
 	if (writeSize > (m_buffSize+m_buffPos)) {	//写入数据大小超过默认buff剩余大小，申请弹性buff"nextbuff_"
 		nextbuff_ = new char[writeSize];
 		memcpy(nextbuff_, writebuff, writeSize);
--- MyHTTP/http/Buffer.h
+++ MyHTTP/http/Buffer.h
@@ -16,15 +16,29 @@ public:
 	~Buffer();
 
 	//operate
-	int read2buff();
-	int write2buff(void* writebuff,int writeSize);
+	int read2fd();							//从套接字fd读入数据到Buffer::buff_
+	int write2fd();							//从Buffer::buff_写入数据到套接字fd
+
+	int readBuff(void* dest, int readSize);
+	int writeBuff(void* writebuff,int writeSize); //写入数据到Buffer::buff_
 
 public:
 	int m_fd;
-	int m_buffPos;			//已存入buff_中的数据大小
+	int m_buffRWPos;		//用户需要读/写buff_数据时的指针
+	int m_buffPos;			//从内核缓冲区写数据到buff_时的指针
+	
+	int m_nextbuffRWPos;
 	int m_nextbuffPos;
+
 	int m_buffSize;			//缓冲区大小
 
+/*
+buff_: 
+					   ↓buffPos		  ↓buffSize
+		[ ][ ][*][*][*][ ][ ][ ][ ][ ][  ][  ]
+		 0  1  2  3  4  5  6  7  8  9  10  11
+			  ↑buffRWPos
+*/
 
 private:
 	char buff_[2048];
